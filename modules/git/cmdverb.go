e3aa07e6c46877835ee83c1732b9c4b24e8df6f5
Tyler Cook <10459406+cilki@users.noreply.github.com>
2026-04-30 21:44:53 -0500

    wip: git-annex SSH support

--- modules/git/cmdverb.go
+++ modules/git/cmdverb.go
@@ -3,12 +3,17 @@
 
 package git
 
+import (
+	"code.gitea.io/gitea/models/perm"
+)
+
 const (
 	CmdVerbUploadPack      = "git-upload-pack"
 	CmdVerbUploadArchive   = "git-upload-archive"
 	CmdVerbReceivePack     = "git-receive-pack"
 	CmdVerbLfsAuthenticate = "git-lfs-authenticate"
 	CmdVerbLfsTransfer     = "git-lfs-transfer"
+	CmdVerbAnnexShell      = "git-annex-shell"
 
 	CmdSubVerbLfsUpload   = "upload"
 	CmdSubVerbLfsDownload = "download"
@@ -20,7 +25,8 @@ func IsAllowedVerbForServe(verb string) bool {
 		CmdVerbUploadArchive,
 		CmdVerbReceivePack,
 		CmdVerbLfsAuthenticate,
-		CmdVerbLfsTransfer:
+		CmdVerbLfsTransfer,
+		CmdVerbAnnexShell:
 		return true
 	}
 	return false
@@ -33,4 +39,19 @@ func IsAllowedVerbForServeLfs(verb string) bool {
 		return true
 	}
 	return false
+}
+
+// IsAllowedVerbForServeAnnex checks if the verb is a git-annex command.
+func IsAllowedVerbForServeAnnex(verb string) bool {
+	return verb == CmdVerbAnnexShell
+}
+
+// AnnexAccessMode returns the access mode required for a git-annex-shell subcommand.
+func AnnexAccessMode(subVerb string) perm.AccessMode {
+	switch subVerb {
+	case "recvkey", "dropkey", "lockcontent", "unlockcontent":
+		return perm.AccessModeWrite
+	default: // configlist, inannex, sendkey, notifychanges, p2pstdio, etc.
+		return perm.AccessModeRead
+	}
 }
